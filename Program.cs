f94353a7d07f2789b3b66a23b2ea585defce5dcf
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-28 17:54:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson12OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\yarin\Desktop\האקר יו לימודים\c# oop\Lesson12OOP\bin\Debug\net6.0\new\yarin13.txt";
+            var destFileName = @"C:\Users\yarin\Desktop\האקר יו לימודים\c# oop\Lesson12OOP\bin\Debug\net6.0\new\move\yarin15.txt";
+            //string data = "bla bla";
+            //File.WriteAllText(path, data);//גם יוצרת את הקובץ וגם כותבת לקובץ
+            //Console.WriteLine(File.ReadAllText(path));
+            // File.Copy(path, destFileName);
+            // File.Move(path, destFileName);
+            //File.Delete(path);
+            
+
+            //if(File.Exists(path))
+            //{
+            //    Console.WriteLine("the file is Exists! ");
+            //    File.WriteAllText()
+            //}
+            //else
+            //{
+            //    var file = File.Create(path);
+            //    file.Close();
+
+            //    Console.WriteLine("file created!");
+            //}
+
+        }
+    }
+}
